b2d4d38944e49fde2f473d83c46c57d72f48cd5b
bachardaowd <bachar.daowd@gmail.com>
2022-11-03 16:29:04 +0100

    adding POJOs. Closes #2.

---
+++ src/main/java/guru/springgramework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springgramework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springgramework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springgramework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
