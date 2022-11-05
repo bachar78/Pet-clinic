dca5ef567ce82df800ab3668f498645628dfa04d
bachardaowd <bachar.daowd@gmail.com>
2022-11-05 20:13:09 +0100

    Refactor services to common Base Interface. closes #12

--- sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/services/OwnerService.java
+++ sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/services/OwnerService.java
@@ -2,12 +2,6 @@ package guru.springgramework.sfgpetclinic.services;
 
 import guru.springgramework.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLstName(String lastName);
-
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
