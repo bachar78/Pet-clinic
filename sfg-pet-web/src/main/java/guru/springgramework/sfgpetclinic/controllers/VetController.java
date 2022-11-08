22712e4bcea7eb8c01446aa6bd50d00fdfe6c789
bachardaowd <bachar.daowd@gmail.com>
2022-11-08 02:56:08 +0100

    List all vets on index page. closes #16

--- sfg-pet-web/src/main/java/guru/springgramework/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-web/src/main/java/guru/springgramework/sfgpetclinic/controllers/VetController.java
@@ -1,12 +1,21 @@
 package guru.springgramework.sfgpetclinic.controllers;
 
+import guru.springgramework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "vets/index.html"})
-    public String vetsList() {
+    public String vetsList(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
