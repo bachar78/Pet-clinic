bd775ccb69e2454b6de32592eda1bec4787241cb
bachardaowd <bachar.daowd@gmail.com>
2022-11-10 16:16:24 +0100

    Create PetType Map Service. closes #31

---
+++ sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springgramework.sfgpetclinic.services;
+
+import guru.springgramework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
--- sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/services/map/AbstractMapService.java
+++ sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/services/map/AbstractMapService.java
@@ -4,7 +4,7 @@ import guru.springgramework.sfgpetclinic.model.BaseEntity;
 
 import java.util.*;
 
-public class AbstractMapService<T extends BaseEntity, ID extends Long> {
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
     protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
