ad92e0d84866d1f4248ad2ddead207ff6c7b2931
bachardaowd <bachar.daowd@gmail.com>
2022-11-11 09:30:28 +0100

    Crate Specialty Map Service. closes #33

---
+++ sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springgramework.sfgpetclinic.services;
+
+import guru.springgramework.sfgpetclinic.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
