109f080cccfbd9db06ee6c2ff74f2170d887688e
bachardaowd <bachar.daowd@gmail.com>
2022-11-16 14:52:43 +0100

    Create Visit Entity, closes #40

--- sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/model/Pet.java
+++ sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package guru.springgramework.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,6 +23,8 @@ public class Pet extends BaseEntity {
     @Column(name = "birth_date")
     private LocalDate birthDate;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
--- sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/model/Visit.java
+++ sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/model/Visit.java
@@ -1,12 +1,29 @@
 package guru.springgramework.sfgpetclinic.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
+    @DateTimeFormat(pattern = "yyy-mm-dd")
     private LocalDate date;
+
+    @Column(name = "description")
     private String description;
+
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
+
+    public Visit() {
+        this.date = LocalDate.now();
+    }
 
     public LocalDate getDate() {
         return date;
