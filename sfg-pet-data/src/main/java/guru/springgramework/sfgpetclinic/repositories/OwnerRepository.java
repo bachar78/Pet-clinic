11e8dca6d98878a0ea74a8467fc35d2accb72d38
bachardaowd <bachar.daowd@gmail.com>
2022-11-16 16:41:07 +0100

    Add Spring Data JPA Owner Service, closes #42

--- sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/repositories/OwnerRepository.java
+++ sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,8 @@ package guru.springgramework.sfgpetclinic.repositories;
 import guru.springgramework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 
 }
--- sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/repositories/SpecialtyRepository.java
+++ sfg-pet-data/src/main/java/guru/springgramework/sfgpetclinic/repositories/SpecialtyRepository.java
@@ -3,5 +3,6 @@ package guru.springgramework.sfgpetclinic.repositories;
 import guru.springgramework.sfgpetclinic.model.Speciality;
 import org.springframework.data.repository.CrudRepository;
 
+
 public interface SpecialtyRepository extends CrudRepository<Speciality, Long> {
 }
